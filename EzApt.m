d3e828f90104bad469c6419998c97fed84a7b8d4
MidnightChip <34963874+midnightchip@users.noreply.github.com>
2018-03-28 08:31:50 -0600

    Made an actual exec

--- EzApt.m
+++ EzApt.m
@@ -1,38 +1,20 @@
-#import "EzApt.h"
-#import <spawn.h>
-#import <sys/wait.h>
-
-@interface UIImage ()
-+ (UIImage *)imageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
-@end
-
-@implementation EzApt
-- (UIImage *)iconGlyph {
-	return [UIImage imageNamed:@"Icon" inBundle:[NSBundle bundleForClass:[self class]]];
-}
-
-- (UIColor *)selectedColor {
-	return [UIColor greenColor];
-}
-
-- (BOOL)isSelected {
-	return self.EzApt;
-}
-
-- (void)setSelected:(BOOL)selected {
-	self.EzApt = selected;
-	[super refreshState];
-  [self AptGet];
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include <spawn.h>
 
 
-- (void)AptGet {
+void aptGet() {
     pid_t pid;
     int status;
-    const char* args[] = {"aptme.sh", "update", NULL, NULL};
-    posix_spawn(&pid, "/usr/bin/CCModules/aptme", NULL, NULL, (char* const*)args, NULL);
+    const char* args[] = {"apt-get", "update", NULL, NULL};
+    posix_spawn(&pid, "/usr/bin/apt-get", NULL, NULL, (char* const)args, NULL);
     waitpid(pid, &status, WEXITED);
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 30.0, false);
-		self.EzApt = FALSE;
+}
+
+
+int main(int argc, char argv[])
+{
+    aptGet();
+    return 0;
 }
 @end
